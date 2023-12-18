b8f2d4d012c67a03f44146c1b240c4ec2b320188
AmandineS2 <amandine.glaudel@etudiant.univ-reims.fr>
2023-12-18 20:08:46 +0100

    Mythos_v2

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import axios from "axios";
 
-export default function App() {
+function Header() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ height: 427, width: 1500, top:-310, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red' }}>
+      <Text style={{ top: 110 }}>Mon En-tête</Text>
     </View>
   );
 }
 
+function HomeScreen() {
+  return (
+    <View style={{ top: 54, flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'grey' }}>
+      <Header />
+      <Text style={{ top: -94 }}>HomeScreen</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button1} onPress={() => console.log('Bouton 1 cliqué!')}>
+          <Text style={styles.buttonText}>Personnages</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button2} onPress={() => console.log('Bouton 2 cliqué!')}>
+          <Text style={styles.buttonText}>Filtrer</Text>
+        </TouchableOpacity>
+      </View>
+      <Footer />
+    </View>
+  );
+}
+
+function Footer() {
+  return (
+    <View style={{ height: 50, width: 1500, top: 140, alignItems: 'center', justifyContent: 'center', backgroundColor: 'blue' }}>
+      <Text style={{ top: 2 }}>Footer</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+  },
+  button1: {
+    backgroundColor: 'black',
+    padding: 10,
+    margin: 10,
+    width: '45%',
     alignItems: 'center',
-    justifyContent: 'center',
+    top: 50,
+  },
+  button2: {
+    backgroundColor: 'green',
+    padding: 10,
+    margin: 10,
+    width: '45%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
   },
 });
