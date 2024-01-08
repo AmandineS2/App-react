3ad0020f29d2cad2c39d6175469188412f072aae
AmandineS2 <amandine.glaudel@etudiant.univ-reims.fr>
2024-01-08 14:15:07 +0100

    update

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Menu } from 'react-native-paper';
 import axios from "axios";
 
 function Header() {
   return (
-    <View style={{ height: 427, width: 1500, top:-310, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red' }}>
+    <View style={{ height: 427, width: 1500, top:-310, alignItems: 'center', justifyContent: 'center', backgroundColor: '#107EA7' }}>
       <Text style={{ top: 110 }}>Mon En-tête</Text>
     </View>
   );
@@ -14,8 +16,11 @@ function Header() {
 
 function HomeScreen() {
   return (
-    <View style={{ top: 54, flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'grey' }}>
+    <View style={{ top: 54, flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#47567B' }}>
       <Header />
+      <Image
+        style={{width: 100, height: 100}}
+        source={{uri: '../assets/coin!.png'}}/>
       <Text style={{ top: -94 }}>HomeScreen</Text>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button1} onPress={() => console.log('Bouton 1 cliqué!')}>
@@ -25,32 +30,51 @@ function HomeScreen() {
           <Text style={styles.buttonText}>Filtrer</Text>
         </TouchableOpacity>
       </View>
-      <Footer />
     </View>
   );
 }
 
-function Footer() {
+function SearchScreen() {
   return (
-    <View style={{ height: 50, width: 1500, top: 140, alignItems: 'center', justifyContent: 'center', backgroundColor: 'blue' }}>
-      <Text style={{ top: 2 }}>Footer</Text>
+    <View style={{ top: 54, flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#47557B' }}>
+      <Header />
+      <Text style={{ top: -94 }}>Recherche</Text>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+function SearchScreenProfil() {
+  return (
+    <View style={{ top: 54, flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#47557B' }}>
+      <Header />
+      <Text style={{ top: -94 }}>Recherche</Text>
+    </View>
+  );
+}
 
-function App() {
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Accueil" component={HomeScreen} />
+      <Tab.Screen name="Recherche" component={SearchScreen} />
+      <Tab.Screen name="Profil" component={SearchScreenProfil} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <MyTabs />
     </NavigationContainer>
   );
 }
 
-export default App;
+
 
 const styles = StyleSheet.create({
   buttonContainer: {
@@ -59,21 +83,24 @@ const styles = StyleSheet.create({
     width: '60%',
   },
   button1: {
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     padding: 10,
     margin: 10,
     width: '45%',
     alignItems: 'center',
-    top: -50,
+    top: -417,
+    borderRadius: 50,
   },
   button2: {
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     padding: 10,
     margin: 10,
     width: '45%',
     alignItems: 'center',
+    top: -417,
+    borderRadius: 50,
   },
   buttonText: {
-    color: 'white',
+    color: 'black',
   },
 });
